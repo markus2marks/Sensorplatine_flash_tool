876103c263575da1c75656f1e5ee914a4b902758
markus <markus_marks@web.de>
2020-05-10 02:31:18 +0200

    added Kalmanfilter

---
+++ XCsoar_com_if.h
@@ -0,0 +1,36 @@
+/*
+ * XCsoar_com_if.h
+ *
+ *  Created on: May 9, 2020
+ *      Author: markus
+ */
+
+#ifndef XCSOAR_COM_IF_H_
+#define XCSOAR_COM_IF_H_
+
+#include <sys/socket.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <netinet/in.h>
+#include <stdlib.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+class XCsoar_com_if
+{
+	public:
+		int	m_port;
+		int xcsoar_sock;
+		struct sockaddr_in s_xcsoar;
+		XCsoar_com_if(int port);
+		bool connect_to_xcsoar();
+};
+
+
+
+#endif /* XCSOAR_COM_IF_H_ */
